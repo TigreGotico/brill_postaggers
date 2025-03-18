10f6d956341dc84e9de39c71fdeaa760ac8a52a7
miro <jarbasai@mailfence.com>
2025-03-18 08:43:32 +0000

    semver

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ BASEDIR = os.path.abspath(os.path.dirname(__file__))
 
 
 def get_version():
-    version_file = os.path.join(BASEDIR, 'crf_query_xtract', 'version.py')
+    version_file = os.path.join(BASEDIR, 'brill_postaggers', 'version.py')
     major, minor, build, alpha = (None, None, None, None)
     with open(version_file) as f:
         for line in f:
